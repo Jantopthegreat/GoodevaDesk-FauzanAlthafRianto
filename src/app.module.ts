2449b3550dfa141ef70c8a8aaaf9fbc4473f80e4
Jantopthegreat <fauzangriya@gmail.com>
2026-09-25 00:42:45 +0700

    feat: add TicketModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@
    import { ConfigModule } from '@nestjs/config';
    import { PrismaModule } from './prisma/prisma.module';
     import { OrganizationModule } from './organization/organization.module';
+    import { TicketModule } from './ticket/ticket.module';
 
    @Module({
      imports: [ConfigModule.forRoot({ isGlobal: true }), 
       PrismaModule,
       OrganizationModule,
+      TicketModule,
+
 
 
     ],
