da8fb633ff575c98bf3b3d95bb4ecbf459d2efb5
Jantopthegreat <fauzangriya@gmail.com>
2026-09-25 13:28:03 +0700

    feat: update TicketModule to include CacheModule and LlmModule imports

--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { OrganizationModule } from '../organization/organization.module';
 import { TicketController } from './ticket.controller';
 import { TicketService } from './ticket.service';
+import { CacheModule } from '../cache/cache.module';
+import { LlmModule } from '../llm/llm.module';
 
 @Module({
-  imports: [OrganizationModule],
+  imports: [OrganizationModule, CacheModule, LlmModule],
   controllers: [TicketController],
   providers: [TicketService],
 })
